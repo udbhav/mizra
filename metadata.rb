0db2d53d05947380a7d1c4e26da3c60fefabe627
Udbhav Gupta <gupta.udbhav@gmail.com>
2014-06-10 14:13:31 -0400

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@udbhavgupta.com'
 license          'All rights reserved'
 description      'Recipe to provision a Vagrant development machine'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends "apt"
 depends "nginx"
